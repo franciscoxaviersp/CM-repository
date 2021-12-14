c21299412e834aaec573a19081a94c9833fd3190
franciscoxaviersp <45402958+franciscoxaviersp@users.noreply.github.com>
2021-12-14 18:19:22 +0000

    RoomWordSample added

---
+++ RoomWordSample/app/src/main/java/cm/homework/roomwordsample/MainActivity.java
@@ -0,0 +1,67 @@
+package cm.homework.roomwordsample;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WordViewModel mWordViewModel;
+
+    ActivityResultLauncher<Intent> launchActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        //get result
+                        Intent data = result.getData();
+                        Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+                        mWordViewModel.insert(word);
+                    }else{
+                        Toast.makeText(
+                                getApplicationContext(),
+                                R.string.empty_not_saved,
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+
+        mWordViewModel.getAllWords().observe(this, words -> {
+            // Update the cached copy of the words in the adapter.
+            adapter.submitList(words);
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener( view -> {
+            Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+            //launch activity with info to be changed
+            launchActivity.launch(intent);
+        });
+        
+    }
+}
